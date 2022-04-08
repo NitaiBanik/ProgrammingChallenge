407d8572a5088c504b8a1cfba6beb60dc97753c6
Nitai Chandra Banik <nitai.banik@selise.ch>
2022-04-09 01:06:35 +0600

    creating object generator endpoint

--- app.js
+++ app.js
@@ -3,10 +3,23 @@ const app = express();
 
 const port = 6030;
 
+const fileName = "objects.txt";
+
 app.get('/ping', (request, response) => {
     response.send("Ping");
+})
+
+app.get('/generate', (request, response) => {
+    generateObjects();
+    var host = request.get('host');
+    response.writeHead(200, {'Content-Type': 'text/json'});
+    response.end(JSON.stringify({downloadUrl: host + "/" +  fileName}));
 })
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
+
+function generateObjects(){
+    console.log("Generating objects");
+}
