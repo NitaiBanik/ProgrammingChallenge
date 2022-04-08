0bc31fb248815374b361d6b663ea6fe022e8072f
Nitai Chandra Banik <nitai.banik@selise.ch>
2022-04-08 22:19:37 +0600

    file reading implementation

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const  http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const port = 6030;
 
 http.createServer((request, response) => {
-    fs.readFile('objects.txt', (err, data) => {
-        response.writeHead(200, {'Content-Type': 'text/json'});
-        response.write(data);
-        response.end();
-});
+    var params = url.parse(request.url, true);
+    var filename = "." + params.pathname;
+    fs.readFile(filename, function(error, texts) {
+      if (error) {
+        console.log(error);
+        response.writeHead(404, {'Content-Type': 'text/html'});
+        return response.end("404 fffffffNot Found");
+      } 
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(texts);
+      return response.end();
+    });
 }).listen(port, () =>{
     console.log(`Server started on port: ${port}`);
 });
