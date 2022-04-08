ee6630a3f1e8ac5cdbcc2d54f4d33bc42602792a
Nitai Chandra Banik <nitai.banik@selise.ch>
2022-04-09 01:22:08 +0600

    file downalod url generated

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ const express = require('express');
 const app = express();
 
 const port = 6030;
-
 const fileName = "objects.txt";
+
+app.use('/files/download', express.static('files'));
 
 app.get('/ping', (request, response) => {
     response.send("Ping");
 })
 
 app.get('/generate', (request, response) => {
+    
     generateObjects();
+
     var host = request.get('host');
     response.writeHead(200, {'Content-Type': 'text/json'});
-    response.end(JSON.stringify({downloadUrl: host + "/" +  fileName}));
+    response.end(JSON.stringify({downloadUrl: host + "/files/download/" +  fileName}));
 })
 
 app.listen(port, () => {
